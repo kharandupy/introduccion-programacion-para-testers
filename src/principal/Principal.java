9049f6eb8d215846f3d2ba76c686fa09b52c601b
Fernando <nandocy@gmail.com>
2026-05-11 23:35:15 -0300

    primer commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        // Primer linea escrita de codigo
+        System.out.println("Hola mundo!");
+        System.out.println("Nuestro nuevo codigo");
+
+        System.out.print("Esto");
+        System.out.print(" es una prueba de impresion");
+    }
+}
