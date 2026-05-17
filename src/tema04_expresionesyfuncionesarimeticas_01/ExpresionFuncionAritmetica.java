407e9381be95b717deccd9597856db4d05356d33
Fernando <nandocy@gmail.com>
2026-05-17 00:16:59 -0300

    practica Expresiones y funciones aritméticas

---
+++ src/tema04_expresionesyfuncionesarimeticas_01/ExpresionFuncionAritmetica.java
@@ -0,0 +1,34 @@
+package tema04_expresionesyfuncionesarimeticas_01;
+
+
+public class ExpresionFuncionAritmetica {
+    public static void main(String[] args) {
+        int x, y;
+        x = 3;
+        int valor = x+4;
+        System.out.println("El resultado es: " + valor);
+
+        y = 29;
+        int variosOp = x + 12 -y +1;
+        System.out.println("El resultado es: " + variosOp);
+
+        int ecuacion = x + y - 2 * 8;
+        System.out.println("El resultado es: " + ecuacion);
+
+        int parentesis = x + (y -2) * 8;
+        System.out.println("El resultado es: " + parentesis);
+
+        double funcion = 4 - Math.sqrt(4);
+        System.out.println("El resultado de funcion es: " + funcion);
+
+        int divisionEntera = 5 / 2;
+        double divisionReal = 5.0 / 2.0;
+        System.out.println("El resultado de la division entera es: " + divisionEntera);
+        System.out.println("El resultado de la division real es: " + divisionReal);
+
+        int resto = 5 % 2;
+        System.out.println("El resto de la division entre 5 y 2 es: " + resto);
+
+        System.out.println("Errores: " + 2 + 3);
+    }
+}
